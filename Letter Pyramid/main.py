b798260bfcf848e1ba9c984885d1f997c313df86
Agnieszka002 <agnieszkadomagalska00@gmail.com>
2026-03-27 10:57:01 +0100

    main.py

--- Letter Pyramid/main.py
+++ Letter Pyramid/main.py
@@ -1,13 +1,13 @@
-znak=input("Tworzymy piramide liter!\nPodaj literę od której zacząć, im dalej litera w alfabecie tym większa piramida!:\n")
+sign=input("Let's build a letter pyramid!\nEnter a letter to start. The further back the letter is in the alphabet, the bigger the pyramid!:\n")
 
-if znak.isalpha():
-  if znak.isupper():
-   startString = ''.join([chr(i)for i in range(ord('A'),ord(znak)+1)])
+if sign.isalpha():
+  if sign.isupper():
+   startString = ''.join([chr(i)for i in range(ord('A'),ord(sign)+1)])
   else:
-    startString=''.join([chr(i)for i in range(ord('a'),ord(znak)+1)])
+    startString=''.join([chr(i)for i in range(ord('a'),ord(sign)+1)])
   for s in range(0,len(startString)):
     print(startString)
     startString = startString[+1:]
 
 else:
-  print("Podano zły znak")
+  print("The wrong sign was entered.")
